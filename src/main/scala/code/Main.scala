b25fd7ea2b1c74b3a8e00d6e684510d8157d582d
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-10-26 15:33:08 +0530

    8

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -3,6 +3,7 @@ package code
 object Main extends App {
 
   println(Sorter.sort(List(19, 40, 7, 10, 23))(Ord.intOrd))
-  println(Sorter.sort(List("s", "d", "a", "z"))(Ord.intOrd))
-  println(Sorter.sort(List(Option(10), Option.empty[Int], Option(3)))(Ord.intOrd))
+  println(Sorter.sort(List("s", "d", "a", "z"))(Ord.strOrd))
+  println(Sorter.sort(List(Option(10), Option.empty[Int], Option(3)))(Ord.intOptOrd))
+  println(Sorter.sort(List(Option("z"), Option.empty[String], Option("a")))(Ord.strOptOrd))
 }
--- src/main/scala/code/Ord.scala
+++ src/main/scala/code/Ord.scala
@@ -5,5 +5,20 @@ trait Ord[T] {
 }
 
 object Ord {
-  val intOrd: Ord[Int] = (a: Int, b: Int) => a < b
+  val intOrd: Ord[Int]    = (a: Int, b: Int) => a < b
+  val strOrd: Ord[String] = (a: String, b: String) => a < b
+
+  val intOptOrd: Ord[Option[Int]] = (a: Option[Int], b: Option[Int]) =>
+    (a, b) match {
+      case (Some(x), Some(y)) => x < y
+      case (_, None)          => false
+      case (None, _)          => true
+  }
+
+  val strOptOrd: Ord[Option[String]] = (a: Option[String], b: Option[String]) =>
+    (a, b) match {
+      case (Some(x), Some(y)) => x < y
+      case (_, None)          => false
+      case (None, _)          => true
+  }
 }
