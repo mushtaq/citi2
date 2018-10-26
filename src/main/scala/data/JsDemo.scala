9bd04541bc79fd27dfd2f79b022cda5f63ed931a
Mushtaq Ahmed <mushtaq.a@gmail.com>
2018-10-26 13:49:38 +0530

    7

--- src/main/scala/data/JsDemo.scala
+++ src/main/scala/data/JsDemo.scala
@@ -4,13 +4,20 @@ import play.api.libs.json._
 
 object JsDemo {
   def transform(json: JsValue): JsValue = json match {
-    case JsNull             => json
-    case boolean: JsBoolean => json
-    case JsNumber(value)    => JsNumber(value * value)
-    case JsString(value)    => json
-    case JsArray(values)    => JsArray(values.map(x => transform(x)))
-    case JsObject(values)   => JsObject(values.mapValues(x => transform(x)))
+    case JsNull                  => json
+    case boolean: JsBoolean      => json
+    case JsNumber(value)         => JsNumber(value * value)
+    case JsString("urn" :: post) => JsString(s"hello $post")
+    case JsArray(values)         => JsArray(values.map(x => transform(x)))
+    case JsObject(values)        => JsObject(values.mapValues(x => transform(x)))
+    case _                       => json
   }
 
-  def dd() = 10
+  object :: {
+    def unapply(arg: String): Option[(String, String)] = arg.split(":") match {
+      case Array(pre, post) => Some((pre, post))
+      case _                => None
+    }
+  }
+
 }
---
+++ ws/2.sc
@@ -0,0 +1,8 @@
+
+
+case class Person(name: String, age: Int)
+
+
+val person = Person("abc", 33)
+
+val Person(name, age) = person
